f0cfdc3cbab543c08a0112e1a6978d9773aafeb7
Shauli Ravfogel <shauli321@gmail.com>
2020-03-11 12:54:42 -0400

    changing the algorithm to in order to sampe uniformly

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+
 import argparse
 from typing import Tuple
 import numpy as np
@@ -5,42 +6,31 @@ import random
 from sklearn.utils import shuffle
 import copy
 
+import tqdm
 
-class PCFG(object):
 
-    def __init__(self, len_range: Tuple[int, int], add_distractors: bool):
-    
-        self.terminate_prob = 10./(len_range[1] - len_range[0])
-        self.min_len, self.max_len = len_range
-        self.expression = "S"
-        self.rules = ["[S]S"]
-        if add_distractors:
-        
-            self.rules.append("[S]*S")
-            self.rules.append("*[S]S")
-            self.rules.append("[*S]S")
-            self.rules.append("[S*]S")
-        self.recurse()
-        
-    def get_current_len(self):
-    
-        return self.expression.count("[") + self.expression.count("]")  #+ self.expression.count("S")  
-            
-    def recurse(self):
-    
-        # if exceeds min length, randomly choose whether to stop. If exceeds max len, stop.
-        
-        if (self.get_current_len() >= self.min_len and random.random() < self.terminate_prob) or self.get_current_len() + 2 > self.max_len:
-        
-            self.expression = self.expression.replace("S", "") #S -> epsilon
-            return
-        
-        # otherwise, randomly recurse
-        
-        rule = np.random.choice(self.rules)
-        self.expression = rule.replace("S", self.expression)
-        self.recurse()
-        
+# from https://sudonull.com/post/123077-How-to-generate-random-parenthesis-sequences
+def tryAndFix(n):
+  seq  = ['(', ')'] * n
+  random.shuffle(seq)
+  stack   = []
+  result  = []
+  balance = 0
+  prev    = 0
+  for pos in range( len(seq) ):
+    balance += 1 if seq[pos] == '(' else -1
+    if balance == 0:
+      if seq[prev] == '(':
+        result.extend( seq[ prev : pos + 1 ] )
+      else:
+        result.append('(')
+        stack.append( [ ')' if v == '(' else '(' for v in seq[ prev + 1 : pos ] ] )
+      prev = pos + 1   
+  for lst in reversed(stack):
+    result.append(')')
+    result.extend(lst)
+  return result
+
 
 def get_opposite_bracket(bracket):
 
@@ -48,7 +38,7 @@ def get_opposite_bracket(bracket):
     elif bracket == "]": return "["
     elif bracket == "(": return ")"
     elif bracket == ")": return "("
-
+    
 def corrupt_sequence(positive_sequence: str) -> str:
     original = copy.copy(positive_sequence)
     
@@ -89,40 +79,47 @@ def write_to_file(name: str, X: list, Y: list):
         for x,y in zip(X,Y):
         
             f.write(x + "\t" + str(y) + "\n")
-
-
-
-def main(train_size: int, dev_size: int, train_lengths: Tuple[int, int], dev_lengths: Tuple[int, int], add_distractors: bool):
+            
+            
+            
+def main(train_size: int, dev_size: int, train_lengths: Tuple[int, int], dev_lengths: Tuple[int, int]):
 
     train, dev = [], []
+    num_seqs_per_length_train = train_size // (train_lengths[1] - train_lengths[0])
+    num_seqs_per_length_dev = dev_size // (dev_lengths[1] - dev_lengths[0])
     
-    for i in range(train_size):
-        seq = PCFG(train_lengths, add_distractors).expression
-        train.append(seq)
+    for length in tqdm.tqdm(range(*train_lengths), total = train_lengths[1] - train_lengths[0]):
+    
+        # generate seqs for this lengths
+        for i in range(num_seqs_per_length_train):
         
-    for i in range(dev_size):
-        seq = PCFG(dev_lengths, add_distractors).expression
-        dev.append(seq)
+            seq = "".join(tryAndFix(length))
+            train.append(seq)      
+        
+    for length in tqdm.tqdm(range(*dev_lengths), total = dev_lengths[1] - dev_lengths[0]):
     
-    lengths_train = [len(seq) for seq in train]
-    print("min lengths_train", np.min(lengths_train), "max lengths_train", np.max(lengths_train), "mean lengths_train", np.mean(lengths_train), "std lengths_train", np.std(lengths_train))
-
-    lengths_dev = [len(seq) for seq in dev]
-    print("min lengths_dev", np.min(lengths_dev), "max lengths_dev", np.max(lengths_dev), "mean lengths_dev", np.mean(lengths_dev), "std lengths_dev", np.std(lengths_dev))
+        # generate seqs for this lengths
+        for i in range(num_seqs_per_length_dev):
+        
+            seq = "".join(tryAndFix(length))
+            dev.append(seq)   
     
+    random.shuffle(train)
+    random.shuffle(dev)
     
+    l1, l2 = len(train)//2, len(dev)//2
     
-    for i in range(train_size//2):
+    for i in range(l1):
     
         train[i] = corrupt_sequence(train[i])
 
-    for i in range(dev_size//2):
+    for i in range(l2):
     
         dev[i] = corrupt_sequence(dev[i])
 
    
-    labels_train = [0] * (train_size//2) + [1] * (train_size//2)
-    labels_dev = [0] * (dev_size//2) + [1] * (dev_size//2)
+    labels_train = [0] * l1 + [1] * l1
+    labels_dev = [0] * l2 + [1] * l2
     
     train_x, train_y = shuffle(train, labels_train)
     dev_x, dev_y = shuffle(dev, labels_dev)
@@ -135,30 +132,31 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='balanced brackets generation',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--train-size', dest='train_size', type=int,
-                        default=15000,
+                        default=75000,
                         help='number of training examples to generate')
     parser.add_argument('--dev-size', dest='dev_size', type=int,
-                        default=15000,
+                        default=75000,
                         help='number of dev examples to generate')
     parser.add_argument('--min-len-train', dest='min_len_train', type=int,
-                        default= 8,
+                        default= 30,
                         help='min length in the training set')
     parser.add_argument('--max-len-train', dest='max_len_train', type=int,
-                        default= 30,
+                        default= 100,
                         help='max length in the training set')
     parser.add_argument('--min-len-dev', dest='min_len_dev', type=int,
-                        default= 15,
+                        default= 30,
                         help='min length in the dev set')
     parser.add_argument('--max-len-dev', dest='max_len_dev', type=int,
-                        default= 45,
+                        default= 100,
                         help='min length in the dev set')
-    parser.add_argument('--add-distractors', dest='add_distractors', type=bool,
+
+    parser.add_argument('--double-brackets', dest='double', type=bool,
                         default= True,
-                        help='Whether to add distractor symbols (*)')                                      
+                        help='Whether to use double brackets')                                       
     args = parser.parse_args()
     
+
     train_lens = (args.min_len_train, args.max_len_train)
     dev_lens = (args.min_len_dev, args.max_len_dev)
     
-    main(args.train_size, args.dev_size, train_lens, dev_lens, args.add_distractors)
-    
+    main(args.train_size, args.dev_size, train_lens, dev_lens)
